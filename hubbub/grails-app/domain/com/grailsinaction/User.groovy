44cfdccbc7952b1fcae589aa9dc5ce758349339f
Omar Yafer Garcia Tecpa <omartecpa1@hotmail.com>
2016-06-28 13:48:46 -0500

    Agregados domains del proyecto

---
+++ hubbub/grails-app/domain/com/grailsinaction/User.groovy
@@ -0,0 +1,23 @@
+package com.grailsinaction
+
+class User {
+    String  loginId
+    String  password
+    Date    dateCreated
+    static hasOne = [ profile : Profile ]
+    static hasMany = [ posts : Post, tags : Tag, following : User ]
+
+    static constraints = {
+        loginId     size: 3..20, unique: true, nullable: false
+        password    size: 6..8, nullable: false, validator: { passwd, user ->
+            passwd != user.loginId
+        }
+        profile     nullable: true
+    }
+
+    static mapping = {
+        posts sort:'dateCreated'
+    }
+
+
+}
